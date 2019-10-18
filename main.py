45a0693f31a180c52ed47d4f7630cc263bfb4ddf
Evan Smith <evan.c.smith@ttu.edu>
2019-10-18 17:16:40 -0500

    Camera Integrated

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
+# import Control
+from .camera import Camera
+from datetime import datetime
 
-#import Control
-#import camera
 # todo finish the control library, pull the final version down then import it like a file
+control = None  # todo this is here to prevent errors until control is done
 
+# control = Control()
+cam = Camera()
+
+
+# control.calibrate()
 def main():
-    # control = Control()
-    # cam = Camera()
-    # control.calibrate()
-    # move to offsets for machine x,y
-    # while (control.y_currentPos <= control.endOfY): # range tolerance? range(control.endOfY, control.endOfY + 5)
-    #     while (control.x_currentPos <= control.endOfX):
-    #         cam.startRecording(5, filename)
-    #         control.moveToNextChip
-    #     control.moveUpRow()
-    pass
+    running_flag = True
+    while running_flag:
+        while control.y_currentPos <= control.endOfY:  # should we use range tolerance? range(control.endOfY, control.endOfY + 5)
+            while control.x_currentPos <= control.endOfX:
+                print(f'Recording chip on Array: [{control.currentChipXY[0]}, {control.currentChipXY[1]}]')
+                cam.record(5, returnFilePath(), 10)  # 5 seconds, filename, 10 fps
+                control.moveToNextChip()
+            print("Finished this Row")
+            control.moveUpRow()
+
+        print("Going for another pass")
+        control.home()
+        control.getBackToFirstChip()
+
 
 main()
+
+
+# todo change chipNum param to control.currentChipNum
+def returnFilePath():
+    now = datetime.now()
+    datetime_str = now.strftime("%d/%m/%Y_%H:%M:%S")
+    #todo decide on naming schema with sid
+    return f'{control.currentChipNum}_{datetime_str}_'
